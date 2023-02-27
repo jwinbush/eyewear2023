b3ea42810fde62526eceac061a6841efee677c08
J.M Winbush <jawonwinbush@gmail.com>
2023-02-27 03:33:46 -0500

    Added framer motion component 💨

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './pages/Home';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./pages/Home";
 import "./index.css";
+import { motion as m } from "framer-motion";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <m.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.75 }}
+    >
+      <Home />
+    </m.div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
