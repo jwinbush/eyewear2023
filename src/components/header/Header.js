62742596a68ad63a753193fc2c2db3c8f16e0451
J.M Winbush <jawonwinbush@gmail.com>
2023-02-28 01:07:58 -0500

    Added watches to list of accessories ⌚️

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import $ from "jquery";
+import watch from "../../assets/watch.png";
 
 // import { Link } from 'react-router-dom';
 
@@ -172,6 +173,13 @@ const Header = () => {
         "https://static.theblacktux.com/products/cufflinks-studs/gold-tone-pearl-cufflinks/JY_015_gold_pearl_0083_1812x1875.jpg?trim=0,186&width=461",
       link: "#",
     },
+    {
+      id: 6,
+      title: "Watches",
+      imageSrc:
+        watch,
+      link: "#",
+    },
   ];
 
   return (
@@ -273,31 +281,14 @@ const Header = () => {
                           >
                             <img
                               src={imageSrc}
-                              className="w-full h-full shadow-lg"
+                              className="w-full h-full shadow-lg sm:max-h-[350px] lg:h-[320px]"
                               alt="From theblacktux.com"
                             />
                             <a href={link}>
                               <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden  bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
                             </a>
                           </div>
-                          <div className="mt-1 mb-3 flex items-center justify-between">
-                            <p className="flex items-center text-primary dark:text-primary-400">
-                              <span className="mr-2 w-4">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 24 24"
-                                  fill="currentColor"
-                                  className="h-5 w-5"
-                                >
-                                  <path d="M21.721 12.752a9.711 9.711 0 00-.945-5.003 12.754 12.754 0 01-4.339 2.708 18.991 18.991 0 01-.214 4.772 17.165 17.165 0 005.498-2.477zM14.634 15.55a17.324 17.324 0 00.332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 00.332 4.647 17.385 17.385 0 005.268 0zM9.772 17.119a18.963 18.963 0 004.456 0A17.182 17.182 0 0112 21.724a17.18 17.18 0 01-2.228-4.605zM7.777 15.23a18.87 18.87 0 01-.214-4.774 12.753 12.753 0 01-4.34-2.708 9.711 9.711 0 00-.944 5.004 17.165 17.165 0 005.498 2.477zM21.356 14.752a9.765 9.765 0 01-7.478 6.817 18.64 18.64 0 001.988-4.718 18.627 18.627 0 005.49-2.098zM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 001.988 4.718 9.765 9.765 0 01-7.478-6.816zM13.878 2.43a9.755 9.755 0 016.116 3.986 11.267 11.267 0 01-3.746 2.504 18.63 18.63 0 00-2.37-6.49zM12 2.276a17.152 17.152 0 012.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0112 2.276zM10.122 2.43a18.629 18.629 0 00-2.37 6.49 11.266 11.266 0 01-3.746-2.504 9.754 9.754 0 016.116-3.985z" />
-                                </svg>
-                              </span>
-                              Travels
-                            </p>
-                            <p>
-                              <u>15.07.2021</u>
-                            </p>
-                          </div>
+                          
                         </div>
                       ))}
                     </div>
@@ -359,31 +350,14 @@ const Header = () => {
                           >
                             <img
                               src={imageSrc}
-                              className="w-full h-full sm:max-h-[350px] lg:max-h-[300px]  shadow-lg dark:shadow-black "
+                              className="w-full h-full sm:max-h-[350px] lg:h-[320px]  shadow-lg dark:shadow-black "
                               alt="From theblacktux.com"
                             />
                             <a href={link}>
                               <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden  bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
                             </a>
                           </div>
-                          <div className="mt-1 mb-3 flex items-center justify-between">
-                            <p className="flex items-center text-primary dark:text-primary-400">
-                              <span className="mr-2 w-4">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 24 24"
-                                  fill="currentColor"
-                                  className="h-5 w-5"
-                                >
-                                  <path d="M21.721 12.752a9.711 9.711 0 00-.945-5.003 12.754 12.754 0 01-4.339 2.708 18.991 18.991 0 01-.214 4.772 17.165 17.165 0 005.498-2.477zM14.634 15.55a17.324 17.324 0 00.332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 00.332 4.647 17.385 17.385 0 005.268 0zM9.772 17.119a18.963 18.963 0 004.456 0A17.182 17.182 0 0112 21.724a17.18 17.18 0 01-2.228-4.605zM7.777 15.23a18.87 18.87 0 01-.214-4.774 12.753 12.753 0 01-4.34-2.708 9.711 9.711 0 00-.944 5.004 17.165 17.165 0 005.498 2.477zM21.356 14.752a9.765 9.765 0 01-7.478 6.817 18.64 18.64 0 001.988-4.718 18.627 18.627 0 005.49-2.098zM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 001.988 4.718 9.765 9.765 0 01-7.478-6.816zM13.878 2.43a9.755 9.755 0 016.116 3.986 11.267 11.267 0 01-3.746 2.504 18.63 18.63 0 00-2.37-6.49zM12 2.276a17.152 17.152 0 012.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0112 2.276zM10.122 2.43a18.629 18.629 0 00-2.37 6.49 11.266 11.266 0 01-3.746-2.504 9.754 9.754 0 016.116-3.985z" />
-                                </svg>
-                              </span>
-                              Travels
-                            </p>
-                            <p>
-                              <u>15.07.2021</u>
-                            </p>
-                          </div>
+                          
                         </div>
                       ))}
                     </div>
@@ -428,7 +402,7 @@ const Header = () => {
                   data-te-dropdown-menu-ref
                 >
                   <div className="flex px-6 py-5 lg:px-8 bg-white">
-                    <div className=" grid gap-6 md:grid-cols-5">
+                    <div className=" grid gap-6 md:grid-cols-6">
                       {accessoriesList.map(({ title, imageSrc, link }) => (
                         <div>
                           <p className="block w-full pt-2 font-semibold uppercase">
@@ -441,31 +415,14 @@ const Header = () => {
                           >
                             <img
                               src={imageSrc}
-                              className="w-full h-full shadow-lg"
+                              className="w-full h-full shadow-lg sm:max-h-[350px] lg:h-[320px]"
                               alt="From theblacktux.com"
                             />
                             <a href={link}>
                               <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden  bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
                             </a>
                           </div>
-                          <div className="mt-1 mb-3 flex items-center justify-between">
-                            <p className="flex items-center text-primary dark:text-primary-400">
-                              <span className="mr-2 w-4">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 24 24"
-                                  fill="currentColor"
-                                  className="h-5 w-5"
-                                >
-                                  <path d="M21.721 12.752a9.711 9.711 0 00-.945-5.003 12.754 12.754 0 01-4.339 2.708 18.991 18.991 0 01-.214 4.772 17.165 17.165 0 005.498-2.477zM14.634 15.55a17.324 17.324 0 00.332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 00.332 4.647 17.385 17.385 0 005.268 0zM9.772 17.119a18.963 18.963 0 004.456 0A17.182 17.182 0 0112 21.724a17.18 17.18 0 01-2.228-4.605zM7.777 15.23a18.87 18.87 0 01-.214-4.774 12.753 12.753 0 01-4.34-2.708 9.711 9.711 0 00-.944 5.004 17.165 17.165 0 005.498 2.477zM21.356 14.752a9.765 9.765 0 01-7.478 6.817 18.64 18.64 0 001.988-4.718 18.627 18.627 0 005.49-2.098zM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 001.988 4.718 9.765 9.765 0 01-7.478-6.816zM13.878 2.43a9.755 9.755 0 016.116 3.986 11.267 11.267 0 01-3.746 2.504 18.63 18.63 0 00-2.37-6.49zM12 2.276a17.152 17.152 0 012.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0112 2.276zM10.122 2.43a18.629 18.629 0 00-2.37 6.49 11.266 11.266 0 01-3.746-2.504 9.754 9.754 0 016.116-3.985z" />
-                                </svg>
-                              </span>
-                              Travels
-                            </p>
-                            <p>
-                              <u>15.07.2021</u>
-                            </p>
-                          </div>
+                        
                         </div>
                       ))}
                     </div>
